1186d9ec0ba4e9f8ce040d527792371989a24d05
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-06 12:45:07 +0900

    코드정리

--- app/src/main/java/com/example/aircheck/MainActivity.kt
+++ app/src/main/java/com/example/aircheck/MainActivity.kt
@@ -40,10 +40,7 @@ class MainActivity : AppCompatActivity() {
         initVariables()
         requestLocationPermissions()
 
-
     }
-
-
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
